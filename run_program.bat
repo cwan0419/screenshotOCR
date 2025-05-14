0e8e88ced1566b349508ec22580003de5e49c13b
An Chaewoong <cwan0419@postech.ac.kr>
2025-05-14 11:55:11 +0900

    v1.1
    Chaged file directory to src
    Chaged file path of input and output
    created run_program.bat

--- run_program.bat
+++ run_program.bat
@@ -1,11 +1,6 @@
 @echo off
-REM 가상환경 활성화
-call venv\Scripts\activate.bat
-
-REM python 프로그램 실행
-python main.py
-
-REM 가상환경 비활성화
-call venv\Scripts\deactivate.bat
-
-pause
+cd /d %~dp0
+call venv\Scripts\activate
+python src/main.py
+call venv\Scripts\deactivate
+pause REM
--- src/main.py
+++ src/main.py
@@ -1,17 +1,27 @@
 from OCR import OCRreader
 from csvOut import CSVout
+import os
 
 def main():
     print("포트폴리오 정리 OCR 프로그램입니다.")
     print("made by An Chaewoong, cwan0419@postech.ac.kr")
-    image_path = "images/screenshot.jpg"  # Path to the image file
+
+    img_name = input("이미지 파일의 이름을 입력하세요.(확장자 포함): ")
+    #image_path = "images/" + img_name  # Path to the image file
+    current_path = os.getcwd()
+    image_path = os.path.join(current_path, "images", img_name)  # Path to the image file
+
     screenshotOCR = OCRreader(lang_list=['en','ko'])
+
+    output_path = os.path.join(current_path, "output/output.csv")
     csvMaker = CSVout("output/output.csv")
 
     screenshotOCR.read_image(image_path)  # Load the image
     screenshotOCR.perform_ocr()  # Perform OCR on the image
     final_result = screenshotOCR.process_result()  # Process the OCR results
+
     csvMaker.write(final_result)  # Write the results to a CSV file
+    
     print("OCR이 완료되었습니다.\n output 폴더의 output.csv 파일을 확인하세요.")
 
     # print(final_result)
